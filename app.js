4a44782afb4dd40b0f95d7f4b6b4e031e7c89fc6
NagendraSutliya <108868292+NagendraSutliya@users.noreply.github.com>
2022-09-16 16:36:28 +0530

    Delete a specific article.

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-//jshint
+//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.set('view engine', "ejs");
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extend: true
+  extended: true
 }));
-
 app.use(express.static("public"));
-
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 
@@ -25,12 +23,12 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+///////////////////////////////////Requests Targetting all Articles////////////////////////
 
-/////////////Requests Targetting All Articles.//////////////////////////
 app.route("/articles")
 
 .get(function(req, res){
-  Article.find({}, function(err, foundArticles){
+  Article.find(function(err, foundArticles){
     if (!err) {
       res.send(foundArticles);
     } else {
@@ -47,13 +45,16 @@ app.route("/articles")
   });
 
   newArticle.save(function(err){
-    if (!err) {
+    if (!err){
       res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
     }
   });
 })
 
 .delete(function(req, res){
+
   Article.deleteMany(function(err){
     if (!err){
       res.send("Successfully deleted all articles.");
@@ -63,15 +64,15 @@ app.route("/articles")
   });
 });
 
-
-/////////////Requests Targetting A Specific Article.//////////////////////////
+////////////////////////////////Requests Targetting A Specific Article////////////////////////
 
 app.route("/articles/:articleTitle")
 
 .get(function(req, res){
+
   Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
     if (foundArticle) {
-      res.send(foundArticle)
+      res.send(foundArticle);
     } else {
       res.send("No articles matching that title was found.");
     }
@@ -79,34 +80,50 @@ app.route("/articles/:articleTitle")
 })
 
 .put(function(req, res){
-  Article.updateOne(
+
+  Article.update(
     {title: req.params.articleTitle},
     {title: req.body.title, content: req.body.content},
     {overwrite: true},
     function(err){
-      if (!err) {
-        res.send("Successfully updated article.");
-      } else {
-        res.send("There is error in updating article.");
+      if(!err){
+        res.send("Successfully updated the selected article.");
       }
     }
   );
 })
 
 .patch(function(req, res){
-  Article.updateOne(
+
+  Article.update(
     {title: req.params.articleTitle},
     {$set: req.body},
     function(err){
-      if (!err){
-        res.send("Successfully updated patch article.");
+      if(!err){
+        res.send("Successfully updated article.");
       } else {
-        res.send("There is an error.");
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req, res){
+
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if (!err){
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
       }
     }
   );
 });
 
-app.listen(8001, function() {
-  console.log("Server started on port 8001");
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
